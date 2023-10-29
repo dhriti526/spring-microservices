10ff66d08455cb1f0fc3d8b1ad4db0ee4cad9ab7
Dell Pc <jaindhrit12@gmail.com>
2023-10-29 22:08:01 +0530

    discovery server changes

--- discovery-server/src/main/java/com/dhriti/discoveryServer/DiscoveryServerApplication.java
+++ discovery-server/src/main/java/com/dhriti/discoveryServer/DiscoveryServerApplication.java
@@ -1,2 +1,13 @@
-package com.dhriti.discoveryServer;public class DiscoveryServerApplication {
+package com.dhriti.discoveryServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class DiscoveryServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DiscoveryServerApplication.class, args);
+    }
 }
